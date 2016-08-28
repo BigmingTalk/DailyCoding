3524c581d8965c587f79581188f2ec96af9a7d3a
Bigming <bigmingtalk@gmail.com>
2016-08-28 09:48:46 +0800

    Coding Everyday~

---
+++ src/chapter_2_listproblem_me/Problem_18_InsertNumToCircularList_me.java
@@ -0,0 +1,66 @@
+package chapter_2_listproblem_me;
+
+/**
+ * Created by bigming on 16/8/28.
+ */
+public class Problem_18_InsertNumToCircularList_me {
+    public static class Node{
+        public int value;
+        public Node next;
+
+        public Node(int value){
+            this.value = value;
+        }
+    }
+
+    public static Node insertNum(Node head, int num){
+        Node node = new Node(num);
+        if (head == null){
+            node.next = node;
+            return node;
+        }
+        Node pre = head;
+        Node cur = head.next;
+        while (cur != head){
+            if (pre.value < num && cur.value >= num){
+                break;
+            }
+            pre = pre.next;
+            cur = cur.next;
+        }
+        pre.next = node;
+        node.next = cur;
+        return num < head.value ? node : head;
+    }
+
+
+    public static void printCircularList(Node head) {
+        if (head == null) {
+            return;
+        }
+        System.out.print("Circular List: " + head.value + " ");
+        Node cur = head.next;
+        while (cur != head) {
+            System.out.print(cur.value + " ");
+            cur = cur.next;
+        }
+        System.out.println("-> " + head.value);
+    }
+
+    public static void main(String[] args) {
+        Node head = null;
+        head = insertNum(head, 2);
+        printCircularList(head);
+        head = insertNum(head, 1);
+        printCircularList(head);
+        head = insertNum(head, 4);
+        printCircularList(head);
+        head = insertNum(head, 3);
+        printCircularList(head);
+        head = insertNum(head, 5);
+        printCircularList(head);
+        head = insertNum(head, 0);
+        printCircularList(head);
+
+    }
+}
