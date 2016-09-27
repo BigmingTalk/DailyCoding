8710c704f00951bed9520b6d05af4d573408ff75
Bigming <bigmingtalk@gmail.com>
2016-09-27 10:55:52 +0800

    Coding Everyday~

--- src/chapter_4_recursionanddp_me/Problem_16_LongestConsecutive_me.java
+++ src/chapter_4_recursionanddp_me/Problem_16_LongestConsecutive_me.java
@@ -47,6 +47,5 @@ public class Problem_16_LongestConsecutive_me {
     public static void main(String[] args) {
         int[] arr = { 100, 4, 200, 1, 3, 2 };
         System.out.println(longestConsecutive(arr));
-
     }
 }
